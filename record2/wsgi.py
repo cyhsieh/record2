e06aca23658d94f90000e33600236ae16b1a8abd
aabbabc@pythonAnyWhere <aabbabc12345@24dcaea7d349>
2018-04-01 17:31:50 +0000

    test pull

--- record2/wsgi.py
+++ record2/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for record2 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+path = os.path.expanduser('~/record_env/record2')
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'record2.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "record2.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
