fef5f05e4a1e29378022ae48f3c4588ab5184628
atheq33 <aabbabc12345@gmail.com>
2018-04-06 18:44:25 +0800

    heroku record2

--- record2/prod_settings.py
+++ record2/prod_settings.py
@@ -1,5 +1,5 @@
 from .settings import *
 STATIC_ROOT = 'static'
-SECURE_PROXY_SSL_HEADER = ('HTTPX_FORWERDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWERDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 DEBUG = False
